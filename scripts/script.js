4dcb530611ee4d4540b96abfa00be56e1ac136d2
alyona- <alyona_om@mail.ru>
2014-12-15 17:52:01 +0300

    Магический шар предсказаний в процессе .....

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,23 @@
 var page = {}
 page.answer = new Array();
 page.answer[0] ="Сегодня будет выходной";
-page.answer[1]=""
+page.answer[1]="Получу пятерку без всякого труда.";
+page.answer[2]="Спалят на списывании.";
+page.answer[3]="Буду решать интересную задачу.";
+page.answer[4]="Позвонит Билл Гейтс.";
+page.answer[5]="Гугл мечтает о тебе. :))";
+
+page.str =new String;
+
+
+page.body = document.body;
+page.magicButton =document.createElement('button');
+page.magicButton.className = 'magicButton';
+page.magicText =document.createElement('span');
+page.magicText.className = 'magicText';
+
+page.magicButton.addEventListener('click', function(){
+    
+},false);
+
+
