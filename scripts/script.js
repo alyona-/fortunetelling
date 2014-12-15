83c853ade1d9629910ecf8926f42422a9b39e8dd
alyona- <alyona_om@mail.ru>
2014-12-16 00:32:05 +0300

    В процессе .......

--- scripts/script.js
+++ scripts/script.js
@@ -11,16 +11,26 @@ page.str =new String;
 
 
 page.body = document.body;
+page.magicBall = document.getElementsByClassName('magicBall')[0];
 page.magicButton =document.createElement('button');
 page.magicButton.className = 'magicButton';
 page.magicText =document.createElement('span');
 page.magicText.className = 'magicText';
+page.str = 'Нажми на меня, чтобы узнать свое будущее';
+page.magicButton.innerText = page.str;
+page.magicBall.appendChild(page.magicButton);
 
-
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 
 page.magicButton.addEventListener('click', function(){
-    
+    //alert('работает');
+    page.magicBall.removeChild(page.magicButton);
+    page.magicBall.appendChild(page.magicText);
+   var number= getRandomInt(0, 5);
+    page.magicText.innerText = page.answer[number];
 },false);
 
 
